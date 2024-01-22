dc42ca728462f59622af262d576156b7d781c25f
GabrielFogo <gabrielcostal164@gmail.com>
2024-01-22 09:31:52 -0300

    4° - pequenas mudanças

--- pilha/pilha.cpp
+++ pilha/pilha.cpp
@@ -5,65 +5,65 @@
 
 using namespace std;
 
-    Pilha::Pilha()
-    {
-        Tamanho = 0;
-        Estrutura = new TipoItem[max_itens];
-    }
+Pilha::Pilha()
+{
+    Tamanho = 0;
+    Estrutura = new TipoItem[max_itens];
+}
 
-    Pilha::~Pilha()
-    {
-        delete [] Estrutura;
-    }
+Pilha::~Pilha()
+{
+    delete [] Estrutura;
+}
 
-    bool Pilha::EstaCheia()
-    {
-        return (Tamanho == max_itens);
-    }
+bool Pilha::EstaCheia()
+{
+    return (Tamanho == max_itens);
+}
 
 
-    bool Pilha::EstaVazia()
-    {
-        return (Tamanho == 0);
-    }
+bool Pilha::EstaVazia()
+{
+    return (Tamanho == 0);
+}
 
-    void Pilha::Inserir(TipoItem item)
+void Pilha::Inserir(TipoItem item)
+{
+    if(EstaCheia())
     {
-        if(EstaCheia())
-        {
-            cout << "A pilha esta cheia\n";
-            cout << "Nao é possivel inserir um elementoz\n"; 
-        }else{
-            Estrutura[Tamanho] = item;
-            Tamanho++;
-        }
+        cout << "A pilha esta cheia\n";
+        cout << "Nao é possivel inserir um elementoz\n"; 
+    }else{
+        Estrutura[Tamanho] = item;
+        Tamanho++;
     }
+}
 
-    TipoItem Pilha::Remover()
+TipoItem Pilha::Remover()
+{
+    if (EstaVazia())
     {
-        if (EstaVazia())
-        {
-            cout << "A pilha esta vazia";
-            cout << "Nao é possivel remover itens";
-            return 0;
-        }else{
-            Tamanho--;
-            return Estrutura[Tamanho];
-        }
-        
+        cout << "A pilha esta vazia";
+        cout << "Nao é possivel remover itens";
+        return 0;
+    }else{
+        Tamanho--;
+        return Estrutura[Tamanho];
     }
+    
+}
 
-    void Pilha::Imprimir()
+void Pilha::Imprimir()
+{
+    cout << "Pilha:[";
+    for (int i = 0; i < Tamanho; i++)
     {
-        cout << "[Pilha:";
-        for (int i = 0; i < Tamanho; i++)
-        {
-            cout << Estrutura[i] << " ";
-        }
-        cout << "]\n";
+        cout << Estrutura[i] << " ";
     }
+    cout << "]\n";
+}
 
-    int Pilha::QualTamanho()
-    {
-      return Tamanho;
-    }
+int Pilha::QualTamanho()
+{
+    return Tamanho;
+}
